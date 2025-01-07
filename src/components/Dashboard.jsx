9bdc3bb6554f447fb253d62347c923cc534aa358
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2025-01-07 20:31:52 +0530

    updated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -17,6 +17,7 @@ function Dashboard() {
   if (role === 'admin') return <AdminDashboard />;
   if (role === 'user') return <UserDashboard email={userEmail} />;
 
+  
   return (
     <div className='bg-[#172554]'>
       <h1 className='flex justify-center text-white text-3xl mt-5'>Login For User And Admin </h1>
